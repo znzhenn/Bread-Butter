f238098a6520fd50706339fd762f6bb9ae65e889
Jenn <znzhenn@gmail.com>
2026-02-16 23:20:02 -0500

    BakingSystem

---
+++ Assets/Scripts/Systems/BakingSystem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BakingSystem : MonoBehaviour
+{
+    public DisplaySystem displaySystem;
+
+    public void BakeBread()
+    {
+        if (GameManager.Instance.CurrentState!= GameManager.GameState.EndDay)
+        {
+            return; // no baking at the end of the day
+        }
+        int quality = Random.Range(50, 101);
+        int value = quality/10;
+
+        Bread bread = new Bread("White Loaf", quality, value);
+        displaySystem.AddBread(bread);
+        //prints what bread is added
+            
+    }
+
+}
