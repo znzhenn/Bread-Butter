13ad6f187c2840c31d8bb3bb90f5ad3c2718fd4e
Jenn <znzhenn@gmail.com>
2026-03-02 23:15:18 -0500

    displays breads available in shop

--- Assets/Scripts/Systems/BakingSystem.cs
+++ Assets/Scripts/Systems/BakingSystem.cs
@@ -5,13 +5,16 @@ using UnityEngine.InputSystem;
 public class BakingSystem : MonoBehaviour
 {
     public List<Bread> breadsForSale = new List<Bread>();
+    public int BreadCount => breadsForSale.Count;
     public void BakeBread(Recipe recipe)
     {
+        //Debug.Log("BakeBread was called!");
         float accuracy = RunTimingMinigame(recipe.difficulty);
         float quality = CalculateQuality(accuracy);
 
         Bread newBread = new Bread(recipe, quality);
         breadsForSale.Add(newBread);
+        Debug.Log("Current bread count: " + breadsForSale.Count);
 
         Debug.Log(recipe.recipeName + " has been baked!" + quality + " star quality " + newBread.breadValue + "cost");
     }
--- Notes.md
+++ Notes.md
@@ -72,7 +72,7 @@ This file tracks progress, milestones, changes in development, as well as an add
 - [Unity Tutorial for Beginners](https://www.youtube.com/watch?v=XtQMytORBmM&t=1079s)
 
 
-## Week 5 ()
+## Week 5 (2/24 - 3/2)
 - changes
 - 
 - 
@@ -80,7 +80,7 @@ This file tracks progress, milestones, changes in development, as well as an add
 - 
 **to-do list**
 
-## Week 6 ()
+## Week 6 (3/3 - 3/19)
 - changes
 - 
 - 
