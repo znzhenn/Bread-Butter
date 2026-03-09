f8330412e097ade25627e2aa4691447fa162918d
Jenn <znzhenn@gmail.com>
2026-03-09 00:14:53 -0400

    bread prefab & bakingsystem updated accordingly

--- Assets/BreadFolder/Bread.cs
+++ Assets/BreadFolder/Bread.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-public class Bread
+public class Bread : MonoBehaviour
 {
     public Recipe recipe;
     public float quality;
     public float breadValue;
 
     // constructor
-    public Bread(Recipe recipe, float quality)
+    public void Initialize(Recipe recipe, float quality)
     {
         this.recipe = recipe;
         this.quality = quality;
--- Assets/BreadFolder/Recipe.cs
+++ Assets/BreadFolder/Recipe.cs
@@ -8,6 +8,7 @@ public class Recipe : ScriptableObject
     public string recipeName;
     public int difficulty; // 1-5 stars (1 is lowest - 5 is highest)
     public float baseValue;
+    public Sprite icon;
     // public List<IngredientsRequired> ingredients;
     // public Sprite icon; 
     // public string description; // for more info (and the actual text)
