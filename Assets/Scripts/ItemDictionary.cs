c90092c6f5296e53d1a55e4121748894ea8a78fd
Jenn <znzhenn@gmail.com>
2026-04-28 05:43:29 -0400

    item Dictionary startedd

---
+++ Assets/Scripts/ItemDictionary.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ItemDictionary : MonoBehaviour
+{
+    public List<Item> itemPrefabs;
+    private Dictionary<int, GameObject> itemDictionary;
+
+    private void Awake()
+    {
+        itemDictionary = new  Dictionary<int, GameObject>();
+
+        for (int i = 0; i < itemPrefabs.Count; i++)
+        {
+            if(itemPrefabs[i] != null)
+            {
+                itemPrefabs[i].ID = i + 1;
+            }
+        }
+        
+        foreach(Item item in itemPrefabs)
+        {
+            itemDictionary[item.ID] = item.gameObject;
+        }
+    }
+
+    public GameObject GetItemPrefab(int itemID)
+    {
+        itemDictionary.TryGetValue(itemID, out GameObject prefab);
+        if(prefab == null)
+        {
+            Debug.LogWarning($"Item with ID {itemID} not ound in dictionary");
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
