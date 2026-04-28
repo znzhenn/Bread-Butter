f5eebe8672392208b54b064e64b0f7eb27316891
Jenn <znzhenn@gmail.com>
2026-04-28 03:48:17 -0400

    game saves :')

---
+++ Assets/Scripts/SaveController.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class SaveController : MonoBehaviour
+{
+    private string saveLocation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+        LoadGame();
+        
+    }
+
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundary = FindObjectOfType<CinemachineConfiner2D>().BoundingShape2D.name
+        };
+
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+            FindObjectOfType<CinemachineConfiner2D>().BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+        }
+        else
+        {
+            SaveGame();
+        }
+    }
+}
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+[System.Serializable]
+
+public class SaveData
+{
+    public Vector3 playerPosition;
+    public String mapBoundary; //boundary name for map
+    
+}
